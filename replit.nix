da367819aa8b4b00908de4f9c55264a76454e205
Nawa Manusitthipol <nawa@nawaman.net>
2024-05-12 16:16:17 +0000

    Take from Desktop

--- replit.nix
+++ replit.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
     deps = [
-      pkgs.elmPackages.elm
-      pkgs.nodejs-18_x
+        pkgs.elmPackages.elm
+        pkgs.nodejs-18_x
         pkgs.graalvm17-ce
         pkgs.maven
         pkgs.replitPackages.jdt-language-server
